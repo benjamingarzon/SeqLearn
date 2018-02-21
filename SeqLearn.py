dd926b423759e2ad38a990a488b624e843c2b937
benjamingarzon <benjamin_garzon@yahoo.es>
2018-02-21 16:38:44 +0100

    Added reward bars

--- SeqLearn.py
+++ SeqLearn.py
@@ -5,41 +5,76 @@ Created on Tue Feb 20 08:41:28 2018
 
 @author: benjamin.garzon@gmail.com
 
-Training tool for discrete sequence production
+Training tool for discrete sequence production.
 """
 
 from __future__ import division
 from psychopy import core, visual, event, prefs
-import csv, time
+import time
 from datetime import datetime
-from lib.utils import showStimulus, scorePerformance, Configurator
+from lib.utils import showStimulus, scorePerformance, get_config, startSession
 from generator.generator import Generator
 prefs.general['audioLib'] = ['pygame']
 from psychopy import sound
+import numpy as np
 
-
-config = Configurator().get()
+# start session
+config = get_config()
 
 sess_date = str(datetime.now().date())
 sess_time = str(datetime.now().time())
-sess_num = 1
+sess_num, username, keyswriter, trialswriter, keysfile, trialsfile, maxscore, \
+maxgroupscore = startSession()
 
-buzzer = sound.Sound(config.BUZZER_FILE)
+win = visual.Window(config["SCREEN_RES"], fullscr=False, monitor="testMonitor", 
+                    units="cm")
 
-## Setup Section
-win = visual.Window(config.SCREEN_RES, fullscr=False, monitor="testMonitor", units="cm")
+# define stimuli
+buzzer = sound.Sound(config["BUZZER_FILE"])
+intro_message = visual.TextStim(win, 
+                                text=config["TEXT_INTRO"].format(username, 
+                                           sess_num), 
+                                           height = \
+                                           config["HEADING_TEXT_HEIGHT"], 
+                                           alignHoriz='center') 
+instructions_message = visual.TextStim(win, 
+                                       text=config["TEXT_INSTRUCT"].format(config["MAX_WAIT"]), 
+                                       height = config["TEXT_HEIGHT"], 
+                                       alignHoriz='center') 
+last_label = visual.TextStim(win, 
+                                text=config["LAST_LABEL"], 
+                                height = config["TEXT_HEIGHT"], 
+                                pos = (-3*config["BAR_WIDTH"], 
+                                       -0.5*config["BAR_HEIGHT"]),
+                                alignHoriz='center') 
+best_label = visual.TextStim(win, 
+                             text=config["BEST_LABEL"], 
+                             height = config["TEXT_HEIGHT"],
+                             pos = (0, 
+                                    -0.5*config["BAR_HEIGHT"]),
+                             alignHoriz='center') 
 
-intro_message = visual.TextStim(win, text=config.TEXT_INTRO) 
-error_message = visual.TextStim(win, text=config.TEXT_ERROR) #pos=[0,+3], 
-error_box = visual.Rect(win, height=5, width=5, lineWidth=3, lineColor="red") 
-# turn the text strings into stimuli
+group_best_label = visual.TextStim(win, 
+                             text=config["GROUP_BEST_LABEL"], 
+                             height = config["TEXT_HEIGHT"],
+                             pos = (3*config["BAR_WIDTH"], 
+                                    -0.5*config["BAR_HEIGHT"]),
+                             alignHoriz='center') 
+error_message = visual.TextStim(win, 
+                                text=config["TEXT_ERROR"], 
+                                alignHoriz="center")  
+error_sign = visual.ImageStim(win, 
+                              image=config["WRONG_FILE"]) 
+
 textStimuli = []
-sequence = Generator(set=config.SEQ_KEYS, size=config.SEQ_LENGTH).random()
+sequence = Generator(set=config["SEQ_KEYS"], 
+                     size=config["SEQ_LENGTH"]).random()
 
-for iTrial in range(config.TOTAL_STIMULI):
-    text = config.TEXT_DO_SEQ.format(iTrial, " - ".join(sequence))
+# turn the text strings into stimuli
+for iTrial in range(config["TOTAL_STIMULI"]):
+    text = config["TEXT_DO_SEQ"].format(iTrial+1, " - ".join(sequence))
     textStimuli.append(visual.TextStim(win, text=text)) 
-  
+
 # fixation cross
 fixation = visual.ShapeStim(win, 
     vertices=((0, -0.5), (0, 0.5), (0,0), (-0.5,0), (0.5, 0)),
@@ -47,72 +82,50 @@ fixation = visual.ShapeStim(win,
     closeShape=False,
     lineColor="white"
 )
- 
-# open data output file
-keysfile = open("./data/keysfile.csv", "wb")
-trialsfile = open("./data/trialsfile.csv", "wb")
+     
 
-# connect it with a csv writer
-keyswriter = csv.writer(keysfile, delimiter=";")
-trialswriter = csv.writer(trialsfile, delimiter=";")
-# create output file header
-keyswriter.writerow([
-    "sess_num",
-    "sess_date",    
-    "sess_time",    
-    "cumulative_trial", 
-    "trial", 
-    "keystroke",
-    "key_from",
-    "key_to", 
-    "RT"
-    ])
-trialswriter.writerow([
-    "sess_num",
-    "sess_date",    
-    "sess_time",    
-    "cumulative_trial", 
-    "trial", 
-    "true_sequence", 
-    "obs_sequence", 
-    "accuracy", 
-    "RT",
-    "MT"
-    ])
-    
 ## Experiment Section
 
 #display instructions and wait
 showStimulus(win, intro_message)
+core.wait(config["INTRO_TIME"])
+showStimulus(win, instructions_message)
 
 #check for a keypress
 event.waitKeys()
 
 n = len(textStimuli)
-cum_trial = 0
-trial = 0
-while (trial < n):
+cum_trial = 1 
+trial = 1
+while (trial <= n):
     # present fixation
     showStimulus(win, fixation)
-    core.wait(config.FIXATION_TIME) # note how the real time will be very close to a multiple of the refresh time
-     
+    core.wait(config["FIXATION_TIME"]) 
+    
     # present stimulus text and wait a maximum of 2 second for a response
-    showStimulus(win, textStimuli[trial])    
+    showStimulus(win, textStimuli[trial-1])    
     mytime0 = time.time()
+    timer = core.Clock()
     key_from = ["0"]
     keys = []
     RTs = []
-    for keystroke in range(config.SEQ_LENGTH):
-        key_to = event.waitKeys(keyList=config.ALLOWED_KEYS, )
-        mytime = time.time()
+    
+    
+    for keystroke in range(config["SEQ_LENGTH"]):
+        key_to = event.waitKeys(maxWait=config["MAX_WAIT"], 
+                                keyList=config["ALLOWED_KEYS"], 
+                                timeStamped = timer)
+        #mytime = time.time()
+        print(key_to)
+        print(mytime)
+        print(mytime0)
         RT =  mytime - mytime0
         mytime0 = mytime
         keys.append(key_to)
         RTs.append(RT)
         if key_to=="escape":
             break
-#        print(key)
-#        print(RT)
+
         # write result to data file    
         keyswriter.writerow([
             sess_num,
@@ -125,17 +138,61 @@ while (trial < n):
             key_to, 
             "{:.3f}".format(RT),
         ])
+    
+        key_from = key_to
 
-    accuracy, MT  = scorePerformance(keys, RTs, sequence)
+    accuracy, MT, score  = scorePerformance(keys, RTs, sequence)
     if accuracy < 1:
-        showStimulus(win, error_box)
+        showStimulus(win, error_sign)
         buzzer.play()
-        core.wait(config.ERROR_TIME)
+        core.wait(config["ERROR_TIME"])
         showStimulus(win, error_message)
-        core.wait(config.ERROR_TIME)
+        core.wait(config["ERROR_TIME"])
     else:
-        #feedback and next
+        #feedback
+        maxscore = np.maximum(score, maxscore)
         showStimulus(win, visual.TextStim(win, text="{:.3f}".format(MT)))
+        max_height = maxscore*config["BAR_HEIGHT"]/maxgroupscore
+        last_height = score*config["BAR_HEIGHT"]/maxgroupscore
+        
+        last_bar = visual.Rect(win, height=last_height, 
+                                  width=config["BAR_WIDTH"], 
+                                  lineWidth=0, 
+                                  fillColor="blue", 
+                                  pos=(-3*config["BAR_WIDTH"], 
+                                       0.5*last_height)
+                                  ) 
+        best_bar = visual.Rect(win, height=max_height, 
+                               width=config["BAR_WIDTH"], 
+                               lineWidth=0, 
+                               fillColor="green",
+                               pos=(0, 
+                                    0.5*max_height)
+                               )
+
+        group_best_bar = visual.Rect(win, height=config["BAR_HEIGHT"], 
+                               width=config["BAR_WIDTH"], 
+                               lineWidth=0, 
+                               fillColor="yellow",
+                               pos=(3*config["BAR_WIDTH"], 
+                                    0.5*config["BAR_HEIGHT"])
+                               )
+
+        last_bar.draw()
+        last_label.draw()
+        
+        best_bar.draw()
+        best_label.draw()
+        
+        group_best_bar.draw()
+        group_best_label.draw()
+        
+        win.flip()
+        core.wait(config["FEEDBACK_TIME"])
+
+#        best_group_bar = visual.Rect(win, height=5, width=config.BAR_WIDTH, 
+#                                  lineWidth=3, lineColor="blue")         
+        config["ALLOWED_KEYS"]
         trial = trial + 1
 
     # write result to data file
@@ -150,10 +207,11 @@ while (trial < n):
             accuracy, 
             "{:.3f}".format(RTs[0]),
             "{:.3f}".format(MT),
+            "{:.3f}".format(score)
     ])
 
     cum_trial = cum_trial + 1    
-    core.wait(config.FIXATION_TIME) 
+    core.wait(config["FIXATION_TIME"]) 
 #    if key==None:
 #        key=[]
 #        key.append("no key")
--- lib/utils.py
+++ lib/utils.py
@@ -6,39 +6,24 @@ Created on Tue Feb 20 08:41:28 2018
 
 Some functions for SeqLearn.
 """
-import os, json
+import os, json, csv
 import numpy as np
+import pandas as pd
+from psychopy import gui
 
-class Configurator:
-    """ Create a config."""
+def get_config(config_file=None):
+    if config_file == None:
+        config_file = "./config/config.json"
+    try:
+        config_json = open(config_file, "r")
+        config = json.load(config_json)
+        config_json.close()
+    except IOError: 
+        print "Error: Config_file is missing!"
 
-    def __init__(self, config_file=None):
-        
-        if config_file == None:
-            config_file = "./config/config.json"
-        try:
-            config_json = open(config_file, "r")
-            config_data = json.load(config_json)
-            config_json.close()
-        except IOError: 
-            print "Error: Config_file is missing!"
-
-        self.TEXT_DO_SEQ = config_data["TEXT_DO_SEQ"]
-        self.TEXT_ERROR = config_data["TEXT_ERROR"]
-        self.TEXT_INTRO = config_data["TEXT_INTRO"]
-        self.SEQ_KEYS = config_data["SEQ_KEYS"]
-        self.TOTAL_STIMULI = config_data["TOTAL_STIMULI"]
-        self.SEQ_LENGTH = config_data["SEQ_LENGTH"]
-        self.FIXATION_TIME = config_data["FIXATION_TIME"]
-        self.ERROR_TIME = config_data["ERROR_TIME"]
-        self.ERROR_BOX_TIME = config_data["ERROR_BOX_TIME"]
-        self.ALLOWED_KEYS = self.SEQ_KEYS + ["escape"]  
-        self.BUZZER_FILE = config_data["BUZZER_FILE"]
-        self.SCREEN_RES = config_data["SCREEN_RES"]
-        
-
-    def get(self):           
-        return(self) 
+    config["ALLOWED_KEYS"] = config["SEQ_KEYS"] + ["escape"]  
+    
+    return(config)
 
 def showStimulus(window, stimulus):
     stimulus.draw()
@@ -54,23 +39,78 @@ def scorePerformance(keys, RTs, sequence):
 
     # MT
     MT = np.sum(RTs[1:])
-    return((accuracy, MT))
+    score = 1/MT
+    return((accuracy, MT, score))
 
-def startSession(username):
+def startSession():
     """ 
     Starts a new session.
     """
+    # get username
+    myDlg = gui.Dlg(title="Sequence training.")
+    myDlg.addField('Enter your username:')
+    ok_data = myDlg.show()
+    if myDlg.OK:  
+        username = myDlg.data[0]
+    else:
+        username = 'test0000'
+        
     keysfilename = "./data/keysfile-{}.csv".format(username)
     trialsfilename = "./data/trialsfile-{}.csv".format(username) 
     
-    if os.path.exists(keysfilename) and os.path.exists(trialsfilename) :
+    try: #if os.path.exists(keysfilename) and os.path.exists(trialsfilename) :
+        # proceed from where it was left before
         keysfile = open(keysfilename, "a")
         trialsfile = open(trialsfilename, "a")
-    else:
+        trialstable = pd.read_csv(trialsfilename, sep=';')
+        sess_num = np.max(trialstable["sess_num"]) + 1
+        maxscore = np.max(trialstable["score"])
+        maxgroupscore = maxscore*1.2
+        
+        # connect files with a csv writer
+        keyswriter = csv.writer(keysfile, delimiter=";")
+        trialswriter = csv.writer(trialsfile, delimiter=";")
+
+    except: # else
+        # create new session
         keysfile = open(keysfilename, "wb")
         trialsfile = open(trialsfilename, "wb")
+        sess_num = 1
+        maxscore = 0
+        maxgroupscore = 2
         
-    return(sess_num, keysfile, trialsfile)
+        # connect files with a csv writer
+        keyswriter = csv.writer(keysfile, delimiter=";")
+        trialswriter = csv.writer(trialsfile, delimiter=";")
+
+        # create output file header
+        keyswriter.writerow([
+                "sess_num",
+                "sess_date",    
+                "sess_time",    
+                "cumulative_trial", 
+                "trial", 
+                "keystroke",
+                "key_from",
+                "key_to", 
+                "RT"
+        ])
+        trialswriter.writerow([
+                "sess_num",
+                "sess_date",    
+                "sess_time",    
+                "cumulative_trial", 
+                "trial", 
+                "true_sequence", 
+                "obs_sequence", 
+                "accuracy", 
+                "RT",
+                "MT",
+                "score"
+        ])
+        
+    return(sess_num, username, keyswriter, trialswriter, keysfile, trialsfile,
+           maxscore, maxgroupscore)
 
 
 def filter_keys(keys):
@@ -80,5 +120,3 @@ def filter_keys(keys):
 
     stimulus.draw()
     win.flip()
-
-
