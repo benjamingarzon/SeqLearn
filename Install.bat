a00fe5fb66cff27303a1a6e61f50a18b85ed591f
benjamingarzon <benjamin_garzon@yahoo.es>
2018-10-12 17:29:47 +0200

    Change install

--- Install.bat
+++ Install.bat
@@ -3,7 +3,7 @@
 set /p INSTALLPATH="Enter Installation PATH. Make sure the path does not contain spaces. (Default C:\Users\Public\SeqLearner): " || set INSTALLPATH="C:\Users\Public\SeqLearner"
 md %INSTALLPATH%
 echo Installation path: %INSTALLPATH%
-set /p DOWNLOADPATH="Enter PATH where you unzipped the files. Make sure the path does not contain spaces. (Default %HOMEPATH%\Downloads\SeqLearn-master\SeqLearn-master): " || set DOWNLOADPATH="%HOMEPATH%\Downloads\SeqLearn-master\SeqLearn-master"
+set /p DOWNLOADPATH="Enter PATH where you unzipped the files. (Default C:\%HOMEPATH%\Downloads\SeqLearn-master\SeqLearn-master): " || set DOWNLOADPATH="C:\%HOMEPATH%\Downloads\SeqLearn-master\SeqLearn-master"
 
 echo "Select second copy option (directory)"
 xcopy "%DOWNLOADPATH%" "%INSTALLPATH%\SeqLearn" /E
